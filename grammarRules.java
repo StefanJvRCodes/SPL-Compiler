b0a90ce1627520847ba9e25f18f57c3de5f75ad1
S JvR <s.jvr0829@gmail.com>
2025-09-16 10:26:18 +0200

    Static fix

---
+++ grammarRules.java
@@ -0,0 +1,25 @@
+public class grammarRules {
+    public static void SPL_PROG(){}
+    public static void VARIABLES() {}
+    public static void VAR() {}
+    public static void NAME() {}
+    public static void PROCDEFS() {}
+    public static void PDEF() {}
+    public static void FDEF() {}
+    public static void FUNCDEFS() {}
+    public static void BODY() {}
+    public static void PARAM() {}
+    public static void MAXTHREE() {}
+    public static void MAINPROG() {}
+    public static void ATOM() {}
+    public static void ALGO() {}
+    public static void INSTR() {}
+    public static void ASSIGN() {}
+    public static void LOOP() {}
+    public static void BRANCH() {}
+    public static void OUTPUT() {}
+    public static void INPUT() {}
+    public static void TERM() {}
+    public static void UNOP() {}
+    public static void BINOP() {}
+}
