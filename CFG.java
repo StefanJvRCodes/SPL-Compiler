d52de8f3486afe2e4cdaf71f7cca4002e0cac333
S JvR <s.jvr0829@gmail.com>
2025-09-16 10:23:17 +0200

    Initial function definitions

--- CFG.java
+++ CFG.java
@@ -2,4 +2,29 @@ public class CFG {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public void SPL_PROG(){}
+    public void VARIABLES() {}
+    public void VAR() {}
+    public void NAME() {}
+    public void PROCDEFS() {}
+    public void PDEF() {}
+    public void FDEF() {}
+    public void FUNCDEFS() {}
+    public void BODY() {}
+    public void PARAM() {}
+    public void MAXTHREE() {}
+    public void MAINPROG() {}
+    public void ATOM() {}
+    public void ALGO() {}
+    public void INSTR() {}
+    public void ASSIGN() {}
+    public void LOOP() {}
+    public void BRANCH() {}
+    public void OUTPUT() {}
+    public void INPUT() {}
+    public void TERM() {}
+    public void UNOP() {}
+    public void BINOP() {}
 }
+
