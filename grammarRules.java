e7bf8a4d4fb6af5bab44f971b842662413ec9854
S JvR <s.jvr0829@gmail.com>
2025-09-16 10:45:33 +0200

    Operation validity check

--- grammarRules.java
+++ grammarRules.java
@@ -42,26 +42,104 @@ public class grammarRules {
 
 
     public static void SPL_PROG(){}
+
+
+    
     public static void VARIABLES() {}
+
+
+
     public static void VAR() {}
+
+
+
     public static void NAME() {}
+
+
+
     public static void PROCDEFS() {}
+
+
+
     public static void PDEF() {}
+
+
+
     public static void FDEF() {}
+
+
+
     public static void FUNCDEFS() {}
+
+
+
     public static void BODY() {}
+
+
+
     public static void PARAM() {}
+
+
+
     public static void MAXTHREE() {}
+
+
+
     public static void MAINPROG() {}
+
+
+
     public static void ATOM() {}
+
+
+
     public static void ALGO() {}
+
+
+
     public static void INSTR() {}
+
+
+
     public static void ASSIGN() {}
+
+
+
     public static void LOOP() {}
+
+
+
     public static void BRANCH() {}
+
+
+
     public static void OUTPUT() {}
+
+
+
     public static void INPUT() {}
+
+
+
     public static void TERM() {}
-    public static void UNOP() {}
-    public static void BINOP() {}
+
+
+
+    public static void UNOP(String str) {
+        if (str.equals("neg") || str.equals("not")) {
+            // valid unary operator
+        } else {
+            throw new IllegalArgumentException("Invalid unary operator: " + str);
+        }
+    }
+
+
+
+    public static void BINOP(String str) {
+        if (str.equals("eq") || str.equals(">") || str.equals("or") || str.equals("and") || str.equals("plus") || str.equals("minus") || str.equals("mult") || str.equals("div")) {
+            // valid binary operator
+        } else {
+            throw new IllegalArgumentException("Invalid binary operator: " + str);
+        }
+    }
 }
