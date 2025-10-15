0bc8c43bc0eeba3e1c431dc89c882e5f0c00b424
Angelique Breedt <u23542838@tuks.co.za>
2025-10-15 18:32:02 +0200

    update for SPL_Types

--- SymbolKinds.java
+++ SymbolKinds.java
@@ -1,8 +1,37 @@
+//A updated this file for type information (SPL_Types)
+
 public enum SymbolKinds {
-    GLOBAL_VAR,     // Variables declared in global scope
-    LOCAL_VAR,      // Variables declared in local scope (function/procedure body)
-    PARAMETER,      // Function/procedure parameters
-    MAIN_VAR,       // Variables declared in main scope
-    PROCEDURE,      // Procedure names
-    FUNCTION        // Function names
+    GLOBAL_VAR("numeric"),     // Variables are always numeric
+    LOCAL_VAR("numeric"),      
+    PARAMETER("numeric"),      
+    MAIN_VAR("numeric"),       
+    PROCEDURE("typeless"),     // Procedures are typeless
+    FUNCTION("numeric");       // Functions return numeric values
+
+    private final String defaultType;
+
+    SymbolKinds(String defaultType) {
+        this.defaultType = defaultType;
+    }
+
+    public String getDefaultType() {
+        return defaultType;
+    }
+
+    public boolean isTypeless() {
+        return "typeless".equals(defaultType);
+    }
 }
+
+
+////==========SPL_Scopes Version of SymbolKinds.java============
+// public enum SymbolKinds {
+//     GLOBAL_VAR,     // Variables declared in global scope
+//     LOCAL_VAR,      // Variables declared in local scope (function/procedure body)
+//     PARAMETER,      // Function/procedure parameters
+//     MAIN_VAR,       // Variables declared in main scope
+//     PROCEDURE,      // Procedure names
+//     FUNCTION        // Function names
+// }
+
+
