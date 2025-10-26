03edae47356f19c1ca3ff546579a41d737cae49e
Liamvk1 <u22539761@tuks.co.za>
2025-10-26 11:01:41 +0200

    fixed filepaths

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -7,7 +7,7 @@ echo "======================================================"
 # Compile all Java files
 echo "[1] Compiling Java files..."
 cd ..
-javac -cp "core_compiler:tests" core_compiler/*.java tests/*.java
+javac -cp "core_compiler:tests:spl_types:spl_scopes:code_generation" core_compiler/*.java tests/*.java spl_types/*.java spl_scopes/*.java code_generation/*.java
 cd tests
 
 if [ $? -eq 0 ]; then
@@ -16,7 +16,7 @@ if [ $? -eq 0 ]; then
     
     echo "[2] Running all tests..."
     echo
-    java -cp "../core_compiler:../tests" TestRunner
+    java -cp "../core_compiler:../tests:../spl_types:../spl_scopes:../code_generation" TestRunner
 else
     echo "❌ Java compilation failed!"
     exit 1
